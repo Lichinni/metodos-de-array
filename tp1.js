898773003d68d4a10a92fe0cd05f66bb6e38046e
Lichi <lisandrosgarcia6@gmail.com>
2024-08-07 16:58:16 -0300

    Add files via upload

---
+++ tp1.js
@@ -0,0 +1,28 @@
+var arrayDos=["lapiz", "tijera", "marcador", "regla"]
+
+    document.write(arrayDos.join("<br>"))
+    document.write("<br> ================ <br>")
+
+
+    arrayDos.unshift("hojas A4")
+
+    document.write(arrayDos.join("<br>"))
+    document.write("<br> ================ <br>")
+
+
+    arrayDos.push("cartuchera")
+
+    document.write(arrayDos.join("<br>"))
+    document.write("<br> ================ <br>")
+
+
+    arrayDos.pop()
+
+    document.write(arrayDos.join("<br>"))
+    document.write("<br> ================ <br>")
+
+
+    arrayDos.shift()
+
+    document.write(arrayDos.join("<br>|"))
+    document.write("<br> ================ <br>")
